516ea8a609039873f89ac01baadf6ff608d0b440
Karolina Gucunski <34010776+Liara42@users.noreply.github.com>
2020-09-19 21:00:10 +0200

    JS Added

--- todo.js
+++ todo.js
@@ -388,4 +388,40 @@ const newIngredients = this.ingredients.map((el) => {
   this.ingredients = newIngredients;
 }
 
+// base R
+export const elements = {
+  searchForm: document.querySelector('.search'),
+  searchInput: document.querySelector('.search__field'),
+  searchResList: document.querySelector('.results__list'),
+  searchRes: document.querySelector('.results'),
+  searchResPages: document.querySelector('.results__pages'),
+  recipe: document.querySelector('.recipe'),
+  shopping: document.querySelector('.shopping__list'),
+  likesMenu: document.querySelector('.likes__field'),
+  likesList: document.querySelector('.likes__list'),
+  deleteList: document.querySelector('.delete__list--button'),
+};
+
+export const elementStrings = {
+  loader: 'loader',
+};
+
+export const renderLoader = (parent) => {
+  const loader = `
+    <div class="${elementStrings.loader}">
+        <svg>
+            <use href="img/icons.svg#icon-cw"></use>
+        </svg>
+    </div>
+    `;
+  parent.insertAdjacentHTML('afterBegin', loader);
+};
+
+export const clearLoader = () => {
+  const loader = document.querySelector(`.${elementStrings.loader}`);
+  if (loader) loader.parentElement.removeChild(loader);
+};
+
+//search
+
 //  Add the rest of the JS
