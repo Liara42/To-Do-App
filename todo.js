d3497880049035b2230546f26a0a688f222434e9
Karolina Gucunski <34010776+Liara42@users.noreply.github.com>
2020-09-23 21:34:47 +0200

    Redoing JS in process

--- todo.js
+++ todo.js
@@ -1,3 +1,6 @@
+//  localStorage.setItem('items', JSON.stringify(items));
+//  const storage = JSON.parse(localStorage.getItem('items'));
+
 //Add delete button and add it to each list item
 let nodeList = document.getElementsByTagName('li');
 
@@ -16,7 +19,7 @@ let deleteBtn = document.getElementsByClassName('delete');
 for (let i = 0; i < deleteBtn.length; i++) {
   deleteBtn[i].onclick = function () {
     let div = this.parentElement;
-    div.style.display = 'none';
+    div.parentNode.removeChild(div); 
   };
 }
 
@@ -28,79 +31,34 @@ list.addEventListener(
     if (event.target.tagName === 'LI') {
       event.target.classList.toggle('checked');
     }
-  },
-  false
+  }
 );
 
-//Persist Data in Local Storage
-let persistData = function(){
-  localStorage.setItem('listitem', JSON.stringify(this.item));
-}
+//Add New List Item
+function addNewElement() {
+  let li = document.createElement('li');
+  let inputElement = document.getElementById('inputContent').value;
+  let tn = document.createTextNode(inputElement);
 
-let addItem = function(listItem) {
-  const item = listItem;
-  this.items.push(item);
-
-  //Persist data in localStorage
-  this.persistData();
-
-  return item;
-}
-
-let deleteItem = function(item){
-  
-  this.items.splice(item);
-
-  //Persist data in localStorage
-  this.persistData();
-}
-
-//Read Data from Storage
-let readStorage = function() {
-  const storage = JSON.parse(localStorage.getItem('likes'));
-
-  //Restoring likes from localStorage
-  if (storage) this.likes = storage;
-}
-
-export const deleteLike = (id) => {
-  const el = document.querySelector(`.likes__link[href*="${id}"]`)
-    .parentElement;
-  if (el) el.parentElement.removeChild(el);
-};
-
-//Restore liked recipes and shopping list on page load
-window.addEventListener('load', () => {
-  state.likes = new Likes();
-  state.list = new List();
-  //restore likes and list
-  state.likes.readStorage();
-  state.list.readStorageList();
-  //toggle like menu button
-  likesView.toggleLikeMenu(state.likes.getNumLikes());
-  //render existing likes and shopping list
-  state.likes.likes.forEach((like) => likesView.renderLike(like));
-  state.list.list.forEach((list) => listView.renderItem(list));
-
-
-  //Handling recipe button clicks
-elements.recipe.addEventListener('click', (e) => {
-  if (e.target.matches('.btn-decrease, .btn-decrease *')) {
-    //Decrease button is clicked
-    if (state.recipe.servings > 1) {
-      state.recipe.updateServings('dec');
-      recipeView.updateServingsIngredients(state.recipe);
-    }
-  } else if (e.target.matches('.btn-increase, .btn-increase *')) {
-    //Increase button is clicked
-    state.recipe.updateServings('inc');
-    recipeView.updateServingsIngredients(state.recipe);
-  } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
-    //add ingredients to shopping list
-    controlList();
-  } else if (e.target.matches('.recipe__love, .recipe__love *')) {
-    //like controller
-    controlLike();
+  li.appendChild(tn);
+  if (inputElement === '') {
+    alert('You must enter something');
+  } else {
+    document.getElementById('uList').appendChild(li);
   }
-});
-})
+  document.getElementById('inputContent').value = '';
+
+  let span = document.createElement('span');
+  let text = document.createTextNode('\u00D7');
+
+  span.className = 'delete';
+  span.appendChild(text);
+  li.appendChild(span);
+
+  for (let i = 0; i < deleteBtn.length; i++) {
+    deleteBtn[i].onclick = function () {
+      let div = this.parentElement;
+      div.style.display = 'none';
+    };
+  }
+}
